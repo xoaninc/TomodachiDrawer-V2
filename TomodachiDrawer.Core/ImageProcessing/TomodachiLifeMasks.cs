3c666bc547480ae997aeaf28281d2146c9c5655f
Lucas7yoshi <Lucas7yoshi@gmail.com>
2026-05-21 04:14:26 -0400

    Add the rest of the image names.

--- TomodachiDrawer.Core/ImageProcessing/TomodachiLifeMasks.cs
+++ TomodachiDrawer.Core/ImageProcessing/TomodachiLifeMasks.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TomodachiDrawer.Core.ImageProcessing
+﻿namespace TomodachiDrawer.Core.ImageProcessing
 {
+    // TODO: Move these to an enum and then add a helper method to get the filename from the enum value with a switch.
+    // That way can add [Description] for human readable names and can enumerate them for UI stuff.
     public static class TomodachiLifeMasks
     {
+        // Body Bottoms
+        public const string BodyBottomsPantsH = "bodyBottomsAUgcPantsH_Uia.png";
+        public const string BodyBottomsPantsL = "bodyBottomsAUgcPantsL_Uia.png";
+        public const string BodyBottomsSkirtL = "bodyBottomsAUgcSkirtL_Uia.png";
+        public const string BodyBottomsSkirtS = "bodyBottomsAUgcSkirtS_Uia.png";
+        // Body Tops
+        public const string BodyTopsLongH = "bodyTopslongUgcAlineH_Uia.png"; // TODO: Figure out what H/L/N mean... Then name more appropriately.
+        public const string BodyTopsLongL = "bodyTopslongUgcAlineL_Uia.png";
+        public const string BodyTopsLongN = "bodyTopslongUgcAlineN_Uia.png";
+        public const string BodyTopsDressH = "bodyTopslongUgcDressH_Uia.png";
+        public const string BodyTopsRobeL = "bodyTopslongUgcRobeL_Uia.png";
+        public const string BodyTopsTshirtH = "bodyTopsUgcTshirtH_Uia.png";
+        public const string BodyTopsTshirtL = "bodyTopsUgcTshirtL_Uia.png";
+        public const string BodyTopsTshirtN = "bodyTopsUgcTshirtN_Uia.png";
+        // Goods
         public const string GoodsGame = "GoodsGame00_Uia.png";
-        // todo: all of them 🙃
+        public const string GoodsTV = "GoodsDVD00_Uia.png"; // TV is more intuitive of a name than "dvd"
+        public const string GoodsBook = "GoodsBook00_Uia.png";
+        // Headwear
+        public const string HeadwearCap = "HeadwearUgcCap_Uia.png";
+        public const string HeadwearCostume = "HeadwearUgcCostume_Uia.png";
+        public const string HeadwearHat = "HeadwearUgcHat_Uia.png";
+        // Interior
+        public const string InteriorRoom = "InteriorRoom00_Uia.png";
+        // Object
+        public const string ObjectBall = "ObjectBall00_Uia.png";
+        public const string ObjectCone = "ObjectCone00_Uia.png";
+        public const string ObjectCube = "ObjectCube00_Uia.png";
+        public const string ObjectCylinder = "ObjectCylinder00_Uia.png";
+        public const string ObjectEgg = "ObjectEgg00_Uia.png";
+        public const string ObjectHalfBall = "ObjectHalfBall00_Uia.png";
+        public const string ObjectOctahedron = "ObjectOctahedron00_Uia.png";
+        public const string ObjectPyramid = "ObjectPyramid00_Uia.png";
+        public const string ObjectRoofCone = "ObjectRoofCone00_Uia.png";
+        public const string ObjectRoofDelta = "ObjectRoofDelta00_Uia.png";
+        public const string ObjectRoofDome = "ObjectRoofDome00_Uia.png";
+        public const string ObjectRoofPyramid = "ObjectRoofPyramid00_Uia.png";
     }
 }

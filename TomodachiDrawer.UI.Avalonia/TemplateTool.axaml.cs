fb6ef3724a356aae95874f2d6a6e89a4e484ce0d
Lucas7yoshi <Lucas7yoshi@gmail.com>
2026-05-22 11:15:56 -0400

    Revert windowsclipboardhelper attempt. Windows is screwy, will just add a save/open button in a short bit.

--- TomodachiDrawer.UI.Avalonia/TemplateTool.axaml.cs
+++ TomodachiDrawer.UI.Avalonia/TemplateTool.axaml.cs
@@ -57,12 +57,6 @@ public partial class TemplateTool : Window
 
     private async Task SetClipboardBitmap(Bitmap bitmap)
     {
-        if (OperatingSystem.IsWindows())
-        {
-            WindowsClipboardHelper.SetBitmap(bitmap);
-            return;
-        }
-
         var topLevel = TopLevel.GetTopLevel(this);
         if (topLevel?.Clipboard is { } clipboard)
             await clipboard.SetBitmapAsync(bitmap);

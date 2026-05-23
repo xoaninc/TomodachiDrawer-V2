6ddbb84b503843bbfc264f5a697e52adfb2c4a64
Lucas7yoshi <Lucas7yoshi@gmail.com>
2026-05-22 21:29:48 -0400

    Redistributable detection and warning.

--- TomodachiDrawer.UI.Avalonia/MainWindow.axaml.cs
+++ TomodachiDrawer.UI.Avalonia/MainWindow.axaml.cs
@@ -1,7 +1,3 @@
-using System.Reflection;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
@@ -13,7 +9,13 @@ using Avalonia.Platform.Storage;
 using Avalonia.Styling;
 using Avalonia.Threading;
 
+using Microsoft.Win32;
+
 using SkiaSharp;
+
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 using TomodachiDrawer.Core;
 using TomodachiDrawer.Core.Extensions;
@@ -75,11 +77,28 @@ public partial class MainWindow : Window
         if (CheckForUpdatesCheckBox.IsChecked)
             _ = PerformAsyncUpdateCheck();
 
+        Opened += MainWindow_Opened;
 
-        if (_currentSettings.FirstStartId != CURRENT_WELCOME_ID)
+
+    }
+
+    private bool IsVCRuntimeInstalled()
+    {
+        if (!OperatingSystem.IsWindows())
         {
-            Opened += MainWindow_Opened;
+            return true;
         }
+
+        string keyPath = @"SOFTWARE\Microsoft\VisualStudio\14.0\VC\Runtimes\X64";
+
+        using var key = Registry.LocalMachine.OpenSubKey(keyPath);
+        if (key != null)
+        {
+            var version = key.GetValue("Version")?.ToString();
+            return !string.IsNullOrEmpty(version);
+        }
+
+        return false;
     }
 
     private void InitializeTemplates()
@@ -125,9 +144,24 @@ public partial class MainWindow : Window
 
     private async void MainWindow_Opened(object? sender, EventArgs e)
     {
-        ShowWelcomeMessage();
-        _currentSettings.FirstStartId = CURRENT_WELCOME_ID;
+        if (_currentSettings.FirstStartId != CURRENT_WELCOME_ID)
+        {
+            ShowWelcomeMessage();
+            _currentSettings.FirstStartId = CURRENT_WELCOME_ID;
+        }
         SaveSettings();
+
+        if (!IsVCRuntimeInstalled())
+        {
+            await ShowMessageAsync(
+                "WARNING: MISSING LIBRARIES",
+                $"In order for this program to run, you MUST install the VC Redistributable." +
+                $"\n\nClick the open link button to install it. " +
+                $"If you do not install it, this program will probably crash silently.",
+                new Uri("https://aka.ms/vc14/vc_redist.x64.exe"),
+                "Download Redistributable"
+            );
+        }
     }
 
     // Welcome message stuff. For important changes, the ID is incremented by one by hand whenever something notable changes.
@@ -624,8 +658,10 @@ public partial class MainWindow : Window
                 );
             }
 
+#if !DEBUG
             if (File.Exists(tempPath))
                 File.Delete(tempPath);
+#endif
             totalTime = timingSink.TotalTime;
         });
 
@@ -724,9 +760,10 @@ public partial class MainWindow : Window
                 File.WriteAllBytes(outputPath, uf2Bytes);
                 AppendLog($"Saved UF2 to {outputPath}");
             }
-
+#if !DEBUG
             if (File.Exists(tempPath))
                 File.Delete(tempPath);
+#endif
             totalTime = timingSink.TotalTime;
         });
 
